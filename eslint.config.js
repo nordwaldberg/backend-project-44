26d05c6d496cd5f66080d21243a28aff526e6d6c
nordwaldberg <nord.waldberg@gmail.com>
2025-05-05 20:41:32 +0300

    Update version of ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,10 @@
+import js from '@eslint/js'
 import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import pluginJs from '@eslint/js'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-})
-
-export default [
-  ...compat.extends('airbnb-base'),
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: globals.node,
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'off',
-      'import/extensions': 'off',
-      'import/prefer-default-export': 'off',
-      'import/no-extraneous-dependencies': ['error', { devDependencies: ['eslint.config.js'] }],
-      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
-      'operator-assignment': 'off',
-      'semi': ['error', 'never'],
-      'arrow-parens': 'off',
-      'brace-style': 'off',
-      'quote-props': 'off',
-    },
-  },
-]
+export default defineConfig([
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+])
