fb7c1d4b837a3f53c155544859f5c2623ffd9fbb
nordwaldberg <nord.waldberg@gmail.com>
2025-05-18 16:08:40 +0300

    Change isPrime condition

--- src/utils/predicates.js
+++ src/utils/predicates.js
@@ -5,7 +5,7 @@ const isPrime = (num) => {
     return false
   }
 
-  for (let i = 2; i < Math.floor(num / 2); i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false
     }
